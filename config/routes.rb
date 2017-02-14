34c3c972ba8539208b29c23cbb1ff31e2d2242fc
Benjamin Rosas <ballPointPenguin@gmx.us>
2017-02-14 13:52:02 -0600

    sessions and authorization stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: [:edit, :update]
+  resources :members, except: [:edit, :update]
+
+  post 'login', to: 'sessions#login'
 end
