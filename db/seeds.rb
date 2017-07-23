e35de706f9e657cfe433f1a2a9c2a50283f5fa7c
Steve Tuckner <stevetuckner@gmail.com>
2017-07-23 15:58:07 -0500

    update description for convention

--- db/seeds.rb
+++ db/seeds.rb
@@ -163,7 +163,7 @@ initial_board_race = Race.find_or_create_by(name: 'Initial Board Election Race',
                                             election_id: initial_board_election.id,
                                             filing_deadline_date: Date.new(2017, 8, 5),
                                             candidates_announcement_date: Date.new(2017, 8, 20))
-initial_board_convention = Event.find_or_create_by(name: 'Founding Convention', occurs_at: DateTime.new(2017, 9, 16, 9, 00), location: 'AAAA Theatre, Alexandria, MN')
+initial_board_convention = Event.find_or_create_by(name: 'Founding Convention', occurs_at: DateTime.new(2017, 9, 16, 9, 00), location: 'AAAA Theatre, Alexandria, MN', description: "This convention is to not only launch the organization with its first elected board, but also to bring together people from all around the state to start to take it back to its progressive roots.")
 initial_board_questionnaire = Questionnaire.where(questionnairable_type: 'Race', questionnairable_id: initial_board_race.id).first
 
 # initial_board_questionnaire.destroy if initial_board_questionnaire
