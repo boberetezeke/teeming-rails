c113ef29a55bbcfaa2886156973cdb1168bdf4da
Steve Tuckner <stevetuckner@gmail.com>
2017-09-16 11:04:41 -0500

    remove debug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,6 @@
 class VotesController < ApplicationController
   before_filter :authenticate_user!
 
-  # Election.find(3).races.first.update(vote_start_time: Time.zone.now.change(hour: 16, min: 00), vote_end_time: Time.zone.now.change(hour: 16, min: 30))
   def index
     @race = Race.find(params[:race_id])
     now = Time.zone.now
