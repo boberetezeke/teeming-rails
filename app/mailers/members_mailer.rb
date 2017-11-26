f6669b9c1ffc62691e593426544c3dfa985311fc
Steve Tuckner <stevetuckner@gmail.com>
2017-11-26 17:00:29 -0600

    bcc a given email if it is set in the env

--- app/mailers/members_mailer.rb
+++ app/mailers/members_mailer.rb
@@ -4,8 +4,14 @@ class MembersMailer < ApplicationMailer
   def send_normal(message, from, message_recipient)
     @message = message
     @message_recipient = message_recipient
-    mail(from: from,
-         to: message_recipient.email,
-         subject: message.subject)
+    mail_options = {
+        from: from,
+        to: message_recipient.email,
+        subject: message.subject
+    }
+    if ENV['OUTGOING_MAIL_BCC']
+      mail_options[:bcc] = ENV['OUTGOING_MAIL_BCC']
+    end
+    mail(mail_options)
   end
 end
