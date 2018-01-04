1cefc876e5dae2bfb9403b7b37904b1531117093
Steve Tuckner <stevetuckner@gmail.com>
2018-01-04 03:46:33 -0600

    always show statewide elections

--- app/policies/election_policy.rb
+++ app/policies/election_policy.rb
@@ -33,7 +33,8 @@ class ElectionPolicy < ApplicationPolicy
   end
 
   def show?
-    can_manage_internal_elections? || @user.can_vote_in_election?(@record) || @user.voted_in_election?(@record)
+    can_manage_internal_elections? || @user.can_vote_in_election?(@record) || @user.voted_in_election?(@record) ||
+    (@record.chapter && @record.chapter.is_state_wide)
   end
 
   def new?
