ff45da19a96e2a1c01ce416f4deb2eb9383251ea
Steve Tuckner <stevetuckner@gmail.com>
2018-08-15 21:28:16 -0500

    allow in person selection to add wanting to help out

--- app/models/event_rsvp.rb
+++ app/models/event_rsvp.rb
@@ -6,23 +6,26 @@ class EventRsvp < ApplicationRecord
 
   attr_accessor :during_initialization
 
+  RSVP_TYPE_IN_PERSON_AND_VOLUNTEER = 'in-person-volunteer'
   RSVP_TYPE_IN_PERSON = 'in-person'
   RSVP_TYPE_ONLINE =    'online'
   RSVP_TYPE_NO =        'no'
 
   RSVP_TYPES = {
-      RSVP_TYPE_IN_PERSON => "In person",
-      RSVP_TYPE_ONLINE    => "Online (we will be live streaming and possibly have online voting)",
-      RSVP_TYPE_NO        => "Will not attend"
+      RSVP_TYPE_IN_PERSON_AND_VOLUNTEER =>  "In person and want to help out",
+      RSVP_TYPE_IN_PERSON =>                "In person",
+      RSVP_TYPE_ONLINE    =>                "Online (we will be live streaming and possibly have online voting)",
+      RSVP_TYPE_NO        =>                "Will not attend"
   }
   RSVP_TYPES_DECLARATIVE = {
-      RSVP_TYPE_IN_PERSON => "I will be there in person",
-      RSVP_TYPE_ONLINE    => "I will participate online",
-      RSVP_TYPE_NO        => "I will not attend"
+      RSVP_TYPE_IN_PERSON_AND_VOLUNTEER =>  "I will be there in person and want to help out",
+      RSVP_TYPE_IN_PERSON =>                "I will be there in person",
+      RSVP_TYPE_ONLINE    =>                "I will participate online",
+      RSVP_TYPE_NO        =>                "I will not attend"
   }
 
   scope :for_user,  ->(user) { where(user: user) }
-  scope :in_person, ->{ where(rsvp_type: RSVP_TYPE_IN_PERSON) }
+  scope :in_person, ->{ where(rsvp_type: [RSVP_TYPE_IN_PERSON, RSVP_TYPE_IN_PERSON_AND_VOLUNTEER]) }
   scope :online,    ->{ where(rsvp_type: RSVP_TYPE_ONLINE) }
   scope :no,        ->{ where(rsvp_type: RSVP_TYPE_NO) }
 
